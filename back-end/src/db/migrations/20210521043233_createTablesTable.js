9b553b4e851f5b6678f401fc37dae47493d7c45b
Kristen <kleigh.oconnell@gmail.com>
2021-05-23 01:12:40 -0500

    sync with remote repo

--- back-end/src/db/migrations/20210521043233_createTablesTable.js
+++ back-end/src/db/migrations/20210521043233_createTablesTable.js
@@ -1,20 +1,11 @@
+//This file got deleted after not migrating properly and corrupted the directory
+//I added it back in so I could migrate the other Tables migration
+//It is empty but apparently necessary.
 
 exports.up = function (knex) {
-    return knex.schema.createTable("tables", (table) => {
-        table.increments("table_id").primary();
-        table.string("table_name").notNullable();
-        table.integer("capacity").notNullable();
-        table.string("status").notNullable().defaultTo("Free");
-        table.integer("reservation_id")
-        table.foreign("reservation_id")
-            .references("reservation_id")
-            .inTable("reservations")
-            .onDelete("CASCADE");
-        table.timestamps(true, true);
-    })
+    
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTable("tables");
-  
+
 };
