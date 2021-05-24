023f5ba61064a6fc0494b2472fd84aa3bdb7ebcd
Kristen <kleigh.oconnell@gmail.com>
2021-05-23 19:47:59 -0500

    Revert "Delete 20210521043233_createTablesTable.js"
    This reverts commit a68b8ba8e42bf907c4378918010dfc4ae42c631e.
    
    Merge conflict with local repository.
    
    The commit a68b8ba diverged from the local branch, but remotely.
    I made 4 new local commits and tried to push to the remote origin.
    Merge conflict: this commit does not exist in my local repo.
    (I had to un-delete the file because it corrupted my migration directory
    to have it deleted).
    But since it was deleted remotely, the local branch diverged.
    The last pushed commit on the remote needs to be removed
    so the HEAD can be rebased to point to the last shared commit.
    Then I can push the rest of my local commits (I hope).

--- back-end/src/db/migrations/20210521043233_createTablesTable.js
+++ back-end/src/db/migrations/20210521043233_createTablesTable.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //This file got deleted after not migrating properly and corrupted the directory
 //I added it back in so I could migrate the other Tables migration
 //It is empty but apparently necessary.
@@ -9,3 +10,25 @@ exports.up = function (knex) {
 exports.down = function (knex) {
 
 };
+=======
+
+exports.up = function (knex) {
+    return knex.schema.createTable("tables", (table) => {
+        table.increments("table_id").primary();
+        table.string("table_name").notNullable();
+        table.integer("capacity").notNullable();
+        table.string("status").notNullable().defaultTo("Free");
+        table.integer("reservation_id")
+        table.foreign("reservation_id")
+            .references("reservation_id")
+            .inTable("reservations")
+            .onDelete("CASCADE");
+        table.timestamps(true, true);
+    })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTable("tables");
+  
+};
+>>>>>>> parent of a68b8ba... Delete 20210521043233_createTablesTable.js
