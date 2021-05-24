058390e5de345b9aef9a77bfcb2865255b2300a8
Kristen <kleigh.oconnell@gmail.com>
2021-05-23 20:49:52 -0500

    seat table components

---
+++ front-end/src/tables/SelectTable.js
@@ -0,0 +1,54 @@
+import { useHistory } from "react-router-dom";
+
+function SelectTable({ tables, reservation, handleChange, handleSubmit }) {
+  const history = useHistory();
+  //let capacity = 0;
+
+  const openTables = tables.filter((table) => {
+    table.status === "Free" && table.capacity >= reservation.people;
+  });
+
+  if (!openTables.length) {
+    return (
+      <div>
+        <ErrorAlert
+          error={{
+            message: `There are no tables currently available for a party of ${table.capacity}. Please wait for an open table.`,
+          }}
+        />
+        <button onClick={() => history.push("/")}>Back to Dashboard</button>
+      </div>
+    );
+  } else if (openTables.length) {
+    const tableOptions = openTables.map((table) => {
+      <option value={table.table_name} key={table.table_id}>
+        {table.table_name} - {table.capacity}
+      </option>;
+    });
+
+    return (
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <select
+              className="form-select"
+              id="table_id"
+              name="table_id"
+              onChange={handleChange}
+            >
+              <option value="">Select a Table</option>
+              {tableOptions}
+            </select>
+          </div>
+
+          <div className="row">
+            <button type="submit">Save</button>
+            <button onClick={() => history.goBack()}>Cancel</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SelectTable;
